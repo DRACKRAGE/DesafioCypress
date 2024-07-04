da58128350942ecdcf16768ea10b745006d3ca8b
DRACKRAGE <brunosouzacc@hotmail.com>
2024-07-03 22:11:24 -0300

    gitignore para filtrar arquivos

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,12 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Deve preencher corretamente para cadastro de usuario', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/')
-    cy.contains('a', 'Cadastrar').click();
-    cy.get('input[name="nome"]').type('Teste');
-    cy.get('input[name="email"]').type('teste@email.com');
-    cy.get('input[name="password"]').type('Teste123');
-    cy.get('input[name="confirm_password"]').type('Teste123');
-    cy.contains('button', 'Cadastrar').click();
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Teste');
+    cy.get('[data-test="input-email"]').type('teste@email.com');
+    cy.get('[data-test="input-password"]').type('Teste123');
+    cy.get('[data-test="input-confirm-password"]').type('Teste123');
+    cy.get('[data-test="submit-button"]').click();
 
   })
 })
