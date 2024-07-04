2533bc78d6c48879564d57b4747c328a9fada866
DRACKRAGE <brunosouzacc@hotmail.com>
2024-07-03 21:55:55 -0300

    incrementando linhas

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/')
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name="nome"]').type('Teste');
+    cy.get('input[name="email"]').type('teste@email.com');
+    cy.get('input[name="password"]').type('Teste123');
+    cy.get('input[name="confirm_password"]').type('Teste123');
+    cy.contains('button', 'Cadastrar').click();
+
   })
 })
