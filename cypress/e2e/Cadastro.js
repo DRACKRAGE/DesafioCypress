076d3ec16f414114c96e9c6d9f6926a75c5f4b9a
DRACKRAGE <brunosouzacc@hotmail.com>
2024-07-03 22:16:38 -0300

    mudando nome do arquivo

---
+++ cypress/e2e/Cadastro.js
@@ -0,0 +1,12 @@
+describe('Cadastro de Usuário', () => {
+  it('Deve preencher corretamente para cadastro de usuario', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Teste');
+    cy.get('[data-test="input-email"]').type('teste@email.com');
+    cy.get('[data-test="input-password"]').type('Teste123');
+    cy.get('[data-test="input-confirm-password"]').type('Teste123');
+    cy.get('[data-test="submit-button"]').click();
+
+  })
+})
